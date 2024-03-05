fd461efd3db897ad535d2b6b5209b66361710e3f
Paula Cano <paulacanoroman@gmail.com>
2024-03-05 23:53:21 +0100

    feat: proptype and props
    type PrototypeModalProperties = {
      witdh: string;
      height: string;
      children: ReactNode;
    }
    
    const MyModalButton = ( {witdh= "300px", height= "200px", children,}: PrototypeModalProperties)

--- components/MyModalButton.tsx
+++ components/MyModalButton.tsx
@@ -1,29 +1,35 @@
-import React, { useState } from 'react'
-import styleModal from  './styleModalColor.module.css'
+import React, { ReactNode, useState } from 'react'
+import styleModal from './styleModalColor.module.css'
 
-const MyModalButton = () => {
+type PrototypeModalProperties = {
+  witdh: string;
+  height: string;
+  children: ReactNode;
+}
 
-  
+const MyModalButton = ( {witdh= "300px", height= "200px", children,}: PrototypeModalProperties) => {
+
   const [showModal, setShowModal] = useState<boolean>(false)
 
   const [canBeClose, setCanBeClose] = useState<boolean>(true)
-  
-  const canClose = ()=>{
+
+  const canClose = () => {
     canBeClose && setShowModal(!showModal)
   }
-   
+
   return (
     <>
-    <button onClick={()=>{canClose()}}>open modal</button>
-    {showModal && (
-      <div className={styleModal.modalAllScreenContainer} onClick={canClose}>
-        <div 
-          className={styleModal.modalChildrenContainer}
-          onMouseEnter={()=>{setCanBeClose(false)}}
-          onMouseLeave={()=>{setCanBeClose(true)}}
-        ></div>
-      </div>
-    )}
+      <button onClick={() => { canClose() }}>open modal</button>
+      {showModal && (
+        <div className={styleModal.modalAllScreenContainer} onClick={canClose}>
+          <div
+            className={styleModal.modalChildrenContainer}
+            onMouseEnter={() => { setCanBeClose(false) }}
+            onMouseLeave={() => { setCanBeClose(true) }}
+           
+          ></div>
+        </div>
+      )}
     </>
   )
 }
