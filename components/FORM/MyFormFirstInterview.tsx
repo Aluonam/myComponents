b435f7a467519f8be5288064d98d6740d13da17c
Paula Cano <paulacanoroman@gmail.com>
2024-03-08 19:41:18 +0100

    feat: first style

--- components/FORM/MyFormFirstInterview.tsx
+++ components/FORM/MyFormFirstInterview.tsx
@@ -1,25 +1,31 @@
 import React from 'react'
+import styleMyForm from './MyFormFirstInterview.module.css'
 
 const MyFormFirstInterview = () => {
   return (
     <div>
+        
+        <form className={styleMyForm.boxForm}>
         <h3>Formulario entrevista inicial</h3>
-        <form>
-            <div>
-               <label>Nombre</label>
-               <input type='text'></input> 
-               <label>Apellidos</label>
-               <input type='text'></input> 
+            <div className={styleMyForm.personalDataBox}>
+                <div className={styleMyForm.nameBox}>
+                    <label>Nombre</label>
+                    <input type='text'></input> 
+                    <label>Apellidos</label>
+                    <input type='text'></input> 
+                    <label>Familiares</label>
+                    <input type='text'></input> 
+                </div>
+                <div className={styleMyForm.dateDataBox}> 
+                    <label>Fecha de nacimiento</label>
+                    <input type='date'></input> 
+                    <label>Fecha de entrevista</label>
+                    <input type='date'></input> 
+                    <label>Contacto</label>
+                    <input type='number'></input> 
+                </div>
             </div>
-            <div>
-               <label>Familiares</label>
-               <input type='text'></input> 
-               <label>Fecha de nacimiento</label>
-               <input type='text'></input> 
-               <label>Fecha de entrevista</label>
-               <input type='text'></input> 
-            </div>
-            <div>
+            <div className={styleMyForm.medicalDataBox}>
                <label>Diagnóstico</label>
                <input type='text'></input> 
                <label>Motivo de consulta</label>
