f34516f55001619fd36e80dd08112197ce20e872
Paula Cano <paulacanoroman@gmail.com>
2024-03-08 19:01:15 +0100

    feat: initial form

--- components/FORM/MyFormFirstInterview.tsx
+++ components/FORM/MyFormFirstInterview.tsx
@@ -5,6 +5,28 @@ const MyFormFirstInterview = () => {
     <div>
         <h3>Formulario entrevista inicial</h3>
         <form>
+            <div>
+               <label>Nombre</label>
+               <input type='text'></input> 
+               <label>Apellidos</label>
+               <input type='text'></input> 
+            </div>
+            <div>
+               <label>Familiares</label>
+               <input type='text'></input> 
+               <label>Fecha de nacimiento</label>
+               <input type='text'></input> 
+               <label>Fecha de entrevista</label>
+               <input type='text'></input> 
+            </div>
+            <div>
+               <label>Diagnóstico</label>
+               <input type='text'></input> 
+               <label>Motivo de consulta</label>
+               <input type='text'></input> 
+               <label>Historial previo</label>
+               <input type='text'></input> 
+            </div>
             
         </form>
 
