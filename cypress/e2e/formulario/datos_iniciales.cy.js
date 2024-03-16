98098138a4630309e3def7ae300e5d6913ef1baa
Paula Cano <paulacanoroman@gmail.com>
2024-03-16 20:14:22 +0100

    test: Cypress escribe correctamente input Name

--- cypress/e2e/formulario/datos_iniciales.cy.js
+++ cypress/e2e/formulario/datos_iniciales.cy.js
@@ -5,20 +5,15 @@ describe('example to-do app', () => {
       cy.visit('http://localhost:3000/')
     })
 
-    it('comprobar que el label nombre sale en pantalla', ()=>{
-        cy.contains('Nombre').should('exist')
-    })
-
-    it('comprobar que se escribe nombre' , ()=>{
-        cy.get('#nameFormTest').should('exist')
-    })
-
     it('existe label nombre y existe el input asociado', ()=>{
         cy.contains('Nombre').should('exist')
         cy.get('#nameFormTest').should('exist')
     })
 
-
+    it('escribe correctamente input Name', ()=>{
+        cy.get('#nameFormTest').type('paula')
+        cy.get('#nameFormTest').should('have.value', 'paula')
+    })
     
 
 })
